8a8956da869da3a2d6d5aab86cf4fe0aeae3874e
Thomas Schmidt <tschmidt@suse.de>
2015-12-07 18:19:47 +0100

    render images later

--- app/models/picture_set.rb
+++ app/models/picture_set.rb
@@ -7,28 +7,30 @@ class PictureSet
 
 
   def self.all
-    dirs = Dir.glob(File.join(PICTURE_PATH, "*/*#{ANIMATION_SUFFIX}")).map{|f| f.gsub(PICTURE_PATH + '/', '').gsub(/\/[0-9-_]*#{ANIMATION_SUFFIX}/, '')}
-    dirs.sort.reverse.map{|dir| self.find dir}
+    dirs = Dir.glob(File.join(PICTURE_PATH, "*/*#{ANIMATION_SUFFIX}")).map { |f| f.gsub(PICTURE_PATH + '/', '').gsub(/\/[0-9\-_]*#{ANIMATION_SUFFIX}/, '') }
+    dirs.sort.reverse.map { |dir| self.find dir }
   end
 
   def self.find date
     { path: "picture_sets/#{date}", date: date, animation: "#{date}#{ANIMATION_SUFFIX}",
-      pictures: (1..4).map{|i| { polaroid: "#{date}_#{i}#{POLAROID_SUFFIX}", full: "#{date}_#{i}.jpg" } }
+      pictures: (1..4).map { |i| { polaroid: "#{date}_#{i}#{POLAROID_SUFFIX}", full: "#{date}_#{i}.jpg" } }
     }
   end
 
   def self.create
     date = Time.new.strftime(DATE_FORMAT)
-    angle = -10 + Random.rand(20) + 360
+    angle = -7 + Random.rand(14) + 360
     caption = date
     dir = "#{PICTURE_PATH}/#{date}"
-      Syscall.execute("mkdir #{date}", dir: PICTURE_PATH)
-      (1..4).each do |i|
-        Syscall.execute("gphoto2 --capture-image-and-download --filename #{date}_#{i}.jpg", dir: dir)
-        Syscall.execute("time convert -caption '#{caption}' #{date}_#{i}.jpg -thumbnail 380 -bordercolor Snow -density 100 -gravity center -pointsize 9 -polaroid -#{angle} #{date}_#{i}#{POLAROID_SUFFIX}", dir: dir)
-      end
-      Syscall.execute("time convert -delay 70 #{date}_1#{POLAROID_SUFFIX} #{date}_2#{POLAROID_SUFFIX} #{date}_3#{POLAROID_SUFFIX} #{date}_4#{POLAROID_SUFFIX} #{date}#{ANIMATION_SUFFIX}", dir: dir)
-      self.find date
+    Syscall.execute("mkdir #{date}", dir: PICTURE_PATH)
+    (1..4).each do |i|
+      Syscall.execute("gphoto2 --capture-image-and-download --filename #{date}_#{i}.jpg", dir: dir)
+    end
+    (1..4).each do |i|
+      Syscall.execute("time convert -caption '#{caption}' #{date}_#{i}.jpg -thumbnail 380 -bordercolor Snow -density 100 -gravity center -pointsize 9 -polaroid -#{angle} #{date}_#{i}#{POLAROID_SUFFIX}", dir: dir)
+    end
+    Syscall.execute("time convert -delay 70 #{date}_1#{POLAROID_SUFFIX} #{date}_2#{POLAROID_SUFFIX} #{date}_3#{POLAROID_SUFFIX} #{date}_4#{POLAROID_SUFFIX} #{date}#{ANIMATION_SUFFIX}", dir: dir)
+    self.find date
   end
 
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
 worker_processes 3
-timeout 20
+timeout 60
 preload_app true
 listen "0.0.0.0:3000", :tcp_nopush => true
