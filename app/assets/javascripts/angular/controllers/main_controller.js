32442a99d1a5defa662f569e7e95985dc783f407
Thomas Schmidt <tschmidt@suse.de>
2015-12-08 12:40:28 +0100

    use bootstrap alerts

--- app/assets/javascripts/angular/controllers/main_controller.js
+++ app/assets/javascripts/angular/controllers/main_controller.js
@@ -3,13 +3,14 @@ photoBoothApp.controller('MainCtrl', [
   '$http',
   function ($scope, $http) {
 
+    $scope.alerts = [];
+
     $scope.takePicture = function () {
       $http.post('/picture_sets.json').success(function (data) {
         $scope.picture_sets.unshift(data);
       }).error(function (data) {
-        alert(data.error);
-        $scope.addAlert();
-        //$scope.alerts.push({msg: 'Another alert!'});
+        //alert(data.error);
+        $scope.alerts.push({type: 'danger', msg: data.error});
       })
     };
 
@@ -17,14 +18,13 @@ photoBoothApp.controller('MainCtrl', [
       $http.get('/picture_sets/list.json').success(function (data) {
         $scope.picture_sets = data;
       }).error(function (data) {
-        alert(data.error);
-        $scope.addAlert(data.error);
+        //alert(data.error);
+        $scope.alerts.push({type: 'error', msg: data.error});
       })
     };
 
-    $scope.alerts = [];
-    $scope.addAlert = function () {
-      $scope.alerts.push({msg: 'Another alert!'});
+    $scope.closeAlert = function(index) {
+      $scope.alerts.splice(index, 1);
     };
 
     $scope.update();
