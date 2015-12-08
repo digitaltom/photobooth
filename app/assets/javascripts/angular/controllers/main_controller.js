3ca9f0eeed29f9ef7d94b5a197d18c5a59b6dc4c
Thomas Schmidt <tschmidt@suse.de>
2015-12-08 13:23:54 +0100

    add modal

--- app/assets/javascripts/angular/controllers/main_controller.js
+++ app/assets/javascripts/angular/controllers/main_controller.js
@@ -1,32 +1,55 @@
 photoBoothApp.controller('MainCtrl', [
   '$scope',
   '$http',
-  function ($scope, $http) {
-
+  '$uibModal',
+  '$log',
+  function ($scope, $http, $uibModal, $log) {
     $scope.alerts = [];
 
     $scope.takePicture = function () {
-      $http.post('/picture_sets.json').success(function (data) {
-        $scope.picture_sets.unshift(data);
-      }).error(function (data) {
+      $scope.openShootModal();
+      $http({
+        method: 'POST',
+        url: '/picture_sets.json'
+      }).then(
+        function successCallback(response) {
+        $scope.closeShootModal();
+        $scope.picture_sets.unshift(response.data);
+      }, function errorCallback(response) {
+        $scope.closeShootModal();
         //alert(data.error);
-        $scope.alerts.push({type: 'danger', msg: data.error});
-      })
+        $scope.alerts.push({type: 'danger', msg: response.data.error});
+      }
+      );
     };
 
     $scope.update = function () {
-      $http.get('/picture_sets/list.json').success(function (data) {
-        $scope.picture_sets = data;
-      }).error(function (data) {
-        //alert(data.error);
-        $scope.alerts.push({type: 'error', msg: data.error});
-      })
+      $http({
+        method: 'GET',
+        url: '/picture_sets/list.json'
+      }).then(function successCallback(response) {
+        $scope.picture_sets = response.data;
+      }, function errorCallback(response) {
+        $scope.alerts.push({type: 'error', msg: response.data.error});
+      });
     };
 
-    $scope.closeAlert = function(index) {
+    $scope.closeAlert = function (index) {
       $scope.alerts.splice(index, 1);
     };
+
+    $scope.openShootModal = function () {
+      $scope.shootModalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'picture_shoot.html'
+      });
+    };
+
+    $scope.closeShootModal = function () {
+      $scope.shootModalInstance.dismiss('cancel')
+    }
 
     $scope.update();
 
   }]);
+
