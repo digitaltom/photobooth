606d5cdb59413f2855b536cd316ba9008ab8e0d8
Thomas Schmidt <tschmidt@suse.de>
2018-06-09 00:25:59 +0200

    don't die on sending emails for picture sets without combined image

--- app/mailers/picture_set_mailer.rb
+++ app/mailers/picture_set_mailer.rb
@@ -2,7 +2,9 @@ class PictureSetMailer < ApplicationMailer
 
   def image_email(email, picture_set)
     attachments.inline['animation.gif'] = File.read(File.join(picture_set[:full_path], picture_set[:animation]))
-    attachments.inline['combined.jpg'] = File.read(File.join(picture_set[:full_path], picture_set[:combined]))
+    if File.exist?(File.join(picture_set[:full_path], picture_set[:combined]))
+      attachments.inline['combined.jpg'] = File.read(File.join(picture_set[:full_path], picture_set[:combined]))
+    end
     mail(to: email, subject: "Your Photobox picture from '#{OPTS.image_caption}'")
   end
 end
