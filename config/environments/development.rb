1d30bdbf08a500b1b6f626b5be83dba9e2702bd6
Thomas Schmidt <tschmidt@suse.de>
2018-06-09 00:25:36 +0200

    don't log email content

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -39,8 +36,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.logger = nil
   config.action_mailer.smtp_settings = {
     address: OPTS.mail_settings['address'],
     port: OPTS.mail_settings['port'],
