4d8b7c0f8f0fed04a104b2f1544721e0f3779d82
Thomas Schmidt <tschmidt@suse.de>
2018-06-28 19:21:40 +0200

    suppress gpio errors

--- config/initializers/gpio.rb
+++ config/initializers/gpio.rb
@@ -1,10 +1,13 @@
-# rubocop:disable Output
+# rubocop:disable Output Lint/RescueException
 begin
+  $VERBOSE = nil
   require 'pi_piper'
   GpioPort.on(GpioPort::GPIO_PORTS['READY'])
   puts '*** Running with gpio support'
-rescue StandardError
+rescue Exception
   Object.send(:remove_const, :PiPiper)
   puts '*** Running without gpio support'
+ensure
+  $VERBOSE = nil
 end
 # rubocop:enable Output
