4c82bc4747c9ae0588fe419c24bc6959116e1450
Thomas Schmidt <tschmidt@suse.de>
2018-06-28 19:32:47 +0200

    fix for rubocop

--- config/initializers/gpio.rb
+++ config/initializers/gpio.rb
@@ -1,4 +1,5 @@
-# rubocop:disable Output Lint/RescueException
+# rubocop:disable Output
+# rubocop:disable Lint/RescueException
 begin
   $VERBOSE = nil
   require 'pi_piper'
@@ -11,3 +12,4 @@ ensure
   $VERBOSE = false
 end
 # rubocop:enable Output
+# rubocop:enable Lint/RescueException
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,4 +2,4 @@ worker_processes 3
 timeout 60
 preload_app true
 
-#listen '0.0.0.0', tcp_nopush: true
+# listen '0.0.0.0', tcp_nopush: true
