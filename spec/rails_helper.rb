255d3c262a0abedfed279fd49f814182d78a120a
Thomas Schmidt <tschmidt@suse.de>
2019-08-10 00:23:08 +0200

    test travis setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ require 'selenium/webdriver'
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w[headless disable-gpu] }
+    chromeOptions: { args: %w[no-sandbox headless disable-gpu disable-dev-shm-usage] }
   )
 
   Capybara::Selenium::Driver.new app,
